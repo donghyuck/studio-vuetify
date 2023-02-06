c0fc1363e68611134af7cc74b30989a058c9dc73
donghyuck, son <andang.son@gmail.com>
2023-02-06 21:42:45 +0900

    환경 파일 API 키 사용

--- src/store/unsplash.ts
+++ src/store/unsplash.ts
@@ -6,7 +6,7 @@ import { createApi } from 'unsplash-js'
 //import whatwgFetch from "whatwg-fetch";
 
 const unsplash = createApi({
-  accessKey: 'A1gQ3uxeoBz6WHlaRocOlce1Pg0PHXNrTHU8qM_yoA0',
+  accessKey: import.meta.env.VITE_UNSPLASH_API_KEY,
   //fetch: whatwgFetch,
 })
 
